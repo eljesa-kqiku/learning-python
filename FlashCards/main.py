bbd0e42796293ce00cad5a878c7662cff832a605
Eljesa Kqiku <eljesakqiku@gmail.com>
2023-10-15 01:15:05 +0200

    Day 31: Flip Card Project - Saving unknown words

--- FlashCards/main.py
+++ FlashCards/main.py
@@ -1,20 +1,27 @@
 from tkinter import *
+from pandas.errors import EmptyDataError
 import pandas
 import random
 
+
 BACKGROUND_COLOR = "#B1DDC6"
-
+UNKNOWN_WORDS_PATH = "data/unknown_words.csv"
+ALL_WORDS_PATH = "data/translated_words.csv"
 # ----- Data Management ---------
-data = pandas.read_csv("translated_words.csv")
-all_words = data.to_dict(orient='records')
-
-current_word = None
+try:
+    data = pandas.read_csv(UNKNOWN_WORDS_PATH)
+    all_words = data.to_dict(orient='records')
+except (FileNotFoundError, EmptyDataError):
+    data = pandas.read_csv(ALL_WORDS_PATH)
+    all_words = data.to_dict(orient='records')
+finally:
+    current_word = all_words[0]
 
 
 def generate_new_word():
     global current_word, flip_timer
     window.after_cancel(flip_timer)
-    current_word = all_words[random.choice(all_words)]
+    current_word = random.choice(all_words)
     card.itemconfig(card_image, image=front_img)
     card.itemconfig(language_label, text="Arabic", fill="#000000")
     card.itemconfig(word_label, text=current_word['arabic'], fill="#000000")
@@ -34,8 +41,10 @@ def right_click():
     generate_new_word()
 
 
-def wrong_click():
-    generate_new_word()
+def on_closing():
+    unknown_words = pandas.DataFrame(all_words)
+    unknown_words.to_csv(UNKNOWN_WORDS_PATH, index=False)
+    window.destroy()
 
 
 # ----- UI Setup ---------
@@ -59,10 +68,11 @@ right_btn = Button(image=right_img, highlightthickness=0, bd=0, command=right_cl
 right_btn.grid(column=1, row=1)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0, bd=0, command=wrong_click)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, bd=0, command=generate_new_word)
 wrong_btn.grid(column=0, row=1)
 
 flip_timer = window.after(3000, flip_card)
 generate_new_word()
 
+window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
